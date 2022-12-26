d400f27815337282a01b0bc51f42ded36d529766
Juan Martín García <hello@juangarcia.com.ar>
2022-12-26 15:48:05 -0300

    Add theme switcher

---
+++ js/switchTheme.js
@@ -0,0 +1,19 @@
+const toggle = document.querySelector(".theme-switcher");
+
+console.log(window.matchMedia("(prefers-color-scheme: dark)"))
+
+const storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme) document.documentElement.setAttribute('data-theme', storedTheme);
+
+toggle.onclick = function() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = "light";
+
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  }
+
+  document.documentElement.setAttribute('data-theme', targetTheme)
+  localStorage.setItem('theme', targetTheme);
+};
+
